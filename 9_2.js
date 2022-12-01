3a2b32dd609b94bbf4d384c1cf926313a03e7f2d
hotSMM <sun_dm@mail.ru>
2022-12-01 13:43:41 +0300

    Update 9_2.js

--- 9_2.js
+++ 9_2.js
@@ -1,1 +1,30 @@
+// Модуль 9. Задание 2
+/*
+Вам дана заготовка и результат, который вы должны получить. Ваша задача — написать код, 
+который будет преобразовывать JSON в JS-объект и выводить его в консоль.
+Результат JS-объект:
+{
+  list: [
+    { name: 'Petr', age: 20, prof: 'mechanic' },
+    { name: 'Vova', age: 60, prof: 'pilot' },
+  ]
+}
+*/
 
+let jsonString = `{
+	"list": [
+	 {
+	  "name": "Petr",
+	  "age": "20",
+	  "prof": "mechanic"
+	 },
+	 {
+	  "name": "Vova",
+	  "age": "60",
+	  "prof": "pilot"
+	 }
+	]
+  }`;
+ 
+let data = JSON.parse(jsonString);
+console.log(data);
